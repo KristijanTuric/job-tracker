c77e675c63664476182b52d79c5d37322d659c76
Kristijan Turić <kristijan.turic1@gmail.com>
2026-03-15 16:35:31 +0100

    Style Register page same as Login page

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { register } from "../api/auth";
 import styles from '../styles/register.module.css';
+import { LoadingComponent } from "../components/LoadingComponent";
 
 export function RegisterPage() {
     const nav = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
     async function onSubmit(e: React.SubmitEvent) {
@@ -14,35 +16,34 @@ export function RegisterPage() {
         setError(null);
 
         try {
+            setLoading(true);
             await register({email, password});
             nav("/applications");
         } catch (err) {
             setError(err instanceof Error ? err.message : "Register failed");
+        } finally {
+            setLoading(false);
         }
     }
 
     return (
-        <div className={styles.pageContainer}>
-            <h1>Register</h1>
-            <form onSubmit={onSubmit} className={styles.registerForm}>
-                <label>
-                    Email
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email"/>
-                </label>
-                <label>
-                    Password
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password"/>
-                </label>
-                <button type="submit">Register</button>
-            </form>
+        <div className={styles.rootContainer}>
+            <div className={styles.pageContainer}>
+                <h1>Register</h1>
+                <form onSubmit={onSubmit} className={styles.registerForm}>                    
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email"/>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password"/>
+                    {error && <pre className="error">{error}</pre>}
+                    <button type="submit">Register</button>
+                </form>
 
-            <div>
-                <p>
-                    Have an account? <Link to="/login">Login</Link>
-                </p>
+                <div className={styles.loginLink}>
+                    <p>
+                        Have an account? <Link to="/login">Login</Link>
+                    </p>
+                </div>
+                {loading && <LoadingComponent></LoadingComponent>}
             </div>
-
-            {error && <pre className="error">{error}</pre>}
-        </div>
+        </div>        
     )
 }
