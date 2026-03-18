504d876e4cd30305d84c7bebc3871863054daf42
Kristijan Turić <kristijan.turic1@gmail.com>
2026-03-18 18:30:41 +0100

    Add Contact Update

--- frontend/src/pages/modals/DetailApplicationModal.tsx
+++ frontend/src/pages/modals/DetailApplicationModal.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { formatStatus, type JobApplicationResponse } from "../../api/applications";
 import styles from '../../styles/modal.module.css';
 import contactStyles from '../../styles/contacts.module.css';
-import { deleteContact, listContacts, type ContactResponse } from "../../api/contacts";
+import { deleteContact, listContacts, updateContact, type ContactResponse, type UpdateContactRequest } from "../../api/contacts";
 import { DataLoadingComponent } from "../../components/DataLoadingComponent";
 import { DetailContactModal } from "./Contacts/DetailContactModal";
 import { DeleteApplicationModal } from "./DeleteApplicationModal";
+import { UpdateContactModal } from "./Contacts/UpdateContactModal";
 
 type Props = {
     application: JobApplicationResponse;
@@ -26,6 +27,7 @@ export function DetailApplicationModal({ application, onClose }: Props) {
     const [loading, setLoading] = useState(false);
     const [detailId, setDetailId] = useState<string | null>(null);
     const [deleteId, setDeleteId] = useState<string | null>(null);
+    const [editId, setEditId] = useState<string | null>(null);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
@@ -42,8 +44,11 @@ export function DetailApplicationModal({ application, onClose }: Props) {
             })();
         }, []);
 
-    function handleEditContact() {
-
+    async function handleEditContact(request: UpdateContactRequest): Promise<void> {
+        if (!editId) return;
+        const updated = await updateContact(application.id, editId, request);
+        setContacts((prev) => prev.map((c) => c.id === editId ? updated : c));
+        setEditId(null);
     }
 
     async function handleDeleteContact(): Promise<void> {
@@ -86,7 +91,7 @@ export function DetailApplicationModal({ application, onClose }: Props) {
                             <div className={contactStyles.contactContainer} onClick={() => setDetailId(c.id)}>
                                 <div className={contactStyles.contactName}>{c.name}</div>
                                 <div className={contactStyles.contactActions}>
-                                    <button type="button" onClick={ (e) => {e.stopPropagation(); }}>Ed</button>
+                                    <button type="button" onClick={ (e) => {e.stopPropagation(); setEditId(c.id) }}>Ed</button>
                                     <button type="button" onClick={ (e) => {e.stopPropagation(); setDeleteId(c.id)}}>Del</button>
                                 </div>                                
                             </div>
@@ -102,6 +107,7 @@ export function DetailApplicationModal({ application, onClose }: Props) {
                 {loading && <DataLoadingComponent></DataLoadingComponent>}
                 {detailId && <DetailContactModal contact={contacts.find((c) => c.id === detailId)!} onClose={() => setDetailId(null)}></DetailContactModal>}
                 {deleteId && <DeleteApplicationModal onConfirm={handleDeleteContact} onClose={() => setDeleteId(null)}></DeleteApplicationModal>}
+                {editId && <UpdateContactModal contact={contacts.find((c) => c.id === editId)!} onClose={() => setEditId(null)} onSubmit={handleEditContact}></UpdateContactModal>}
             </div>
         </div>
     )
