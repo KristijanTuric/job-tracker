95ba3e96f9a756e372aea3606e5c68a5b54faf3c
Kristijan Turić <kristijan.turic1@gmail.com>
2026-04-02 18:34:09 +0200

    Use Links for navigation

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import { AtomIcon, HardDrivesIcon } from '@phosphor-icons/react';
 import style from '../styles/home.module.css';
+import { Link } from 'react-router-dom';
 
 export function HomePage() {
     return (
@@ -10,18 +11,18 @@ export function HomePage() {
                     <h1>Job Tracker</h1>
                 </div>
                 <div>
-                    <a className={style.navButton} href='/login'>Login</a>
-                    <a className={style.navButton} href='/register'>Register</a>
+                    <Link className={style.navButton} to='/login'>Login</Link>
+                    <Link className={style.navButton} to='/register'>Register</Link>
                 </div>
             </div>
             <div className={style.homeBody}>
-                <div className={style.homeMessage}><a href='/login'>Login</a> or <a href='/register'>Register</a> to access your<br/> Job Application Tracker</div>
+                <div className={style.homeMessage}><Link to='/login'>Login</Link> or <Link to='/register'>Register</Link> to access your<br/> Job Application Tracker</div>
             </div>    
             <div className={style.homeImage}></div>
             <footer>
                 <HardDrivesIcon size={32}></HardDrivesIcon>
-                <a href="https://github.com/KristijanTuric" title="github link">Made by Kristijan T</a>
-                <a href="https://www.flaticon.com/free-icons/job" title="job icons">Tab Job icon by Freepik - Flaticon</a>
+                <a target='_blank' href="https://github.com/KristijanTuric" title="github link">Made by Kristijan T</a>
+                <a target='_blank' href="https://www.flaticon.com/free-icons/job" title="job icons">Tab Job icon by Freepik - Flaticon</a>
             </footer>
         </div>
     );
